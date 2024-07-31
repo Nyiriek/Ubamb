eb0a1ff18cab3f158a2a1439c4b463ab9ee36064
mulimuoki001 <b.muoki@alustudent.com>
2024-07-31 18:44:00 +0300

    home page modifications

--- ubamb/lib/screens/home_screen.dart
+++ ubamb/lib/screens/home_screen.dart
@@ -99,200 +99,196 @@ class _HomeScreenState extends State<HomeScreen> {
   }
   @override
   Widget build(BuildContext context) {
-    // return Scaffold(
-    //   appBar: AppBar(title: Text('Home')),
-    //   body:
-    // );
+
     return Scaffold(
       backgroundColor: const Color(0xFF4CA6F8),
-      body: RefreshIndicator(
-        onRefresh: _refreshScreen,
+      body: SingleChildScrollView(
 
-          child:  Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const SizedBox(height: 25),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.menu, color: Colors.black),
-                      onPressed: () {
-                        // Implement navigation drawer functionality here
-                      },
-                    ),
-                    const SizedBox(width: 15),
-                    Container(
-                      width: 200,
-                      height: 30,
-                      child: _userInfo != null
-                          ? Text('Welcome, ${_userInfo!['firstName']}!', style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),)
-                          : SmallLoadingIndicator(),
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.person, color: Colors.black),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => AccountScreen()),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 15),
+       child: Padding(padding: EdgeInsets.only(left: 20, right: 20),
+         child: Column(
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: <Widget>[
+           const SizedBox(height: 25),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             children: [
+               IconButton(
+                 icon: const Icon(Icons.menu, color: Colors.black),
+                 onPressed: () {
+                   // Implement navigation drawer functionality here
+                 },
+               ),
+               const SizedBox(width: 15),
+               Container(
+                 width: 200,
+                 height: 30,
+                 child: _userInfo != null
+                     ? Text('Welcome, ${_userInfo!['firstName']}!', style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),)
+                     : SmallLoadingIndicator(),
+               ),
+               IconButton(
+                 icon: const Icon(Icons.person, color: Colors.black),
+                 onPressed: () {
+                   Navigator.push(
+                     context,
+                     MaterialPageRoute(builder: (context) => AccountScreen()),
+                   );
+                 },
+               ),
+             ],
+           ),
+           const SizedBox(height: 15),
 
-                Text("Your current location", style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 16,
-                  color: Color.fromARGB(255, 92, 92, 92),
-                ),),
-                const SizedBox(height: 10),
-                Row(
-                  children: [
-                    Icon(Icons.location_on, color: Colors.blue),
-                    SizedBox(width: 5),
-                    Text(
-                      _address,
-                      style: TextStyle(
-                        fontFamily: 'Hind',
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                    SizedBox(width: 5),
-                    Icon(Icons.arrow_drop_down, color: Colors.black),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  width: 347,
-                  height: 246,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/Group 23.png'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 40),
-                const Text(
-                  'Here for you :)',
-                  style: TextStyle(
-                    fontFamily: 'Karla',
-                    fontSize: 40,
-                    color: Colors.black,
-                  ),
-                ),
-                const SizedBox(height: 35),
-                GestureDetector(
-                  onTap: () {Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => BookRideScreen()),
-                  );
-                  },
-                  child:Container(
-                    width: 269,
-                    height: 48,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(7),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Book a ride with us',
-                        style: TextStyle(
-                          fontFamily: 'Roboto',
-                          fontSize: 25,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+           Text("Your current location", style: TextStyle(
+             fontFamily: 'Roboto',
+             fontSize: 16,
+             color: Color.fromARGB(255, 92, 92, 92),
+           ),),
+           const SizedBox(height: 10),
+           Row(
+             children: [
+               Icon(Icons.location_on, color: Colors.blue),
+               SizedBox(width: 5),
+               Text(
+                 _address,
+                 style: TextStyle(
+                   fontFamily: 'Hind',
+                   fontSize: 15,
+                   fontWeight: FontWeight.bold,
+                   color: Colors.black,
+                 ),
+               ),
+               SizedBox(width: 5),
+               Icon(Icons.arrow_drop_down, color: Colors.black),
+             ],
+           ),
+           const SizedBox(height: 20),
+           Container(
+             width: 347,
+             height: 246,
+             decoration: BoxDecoration(
+               borderRadius: BorderRadius.circular(10),
+               image: DecorationImage(
+                 image: AssetImage('assets/images/Group 23.png'),
+                 fit: BoxFit.cover,
+               ),
+             ),
+           ),
+           const SizedBox(height: 40),
+           const Text(
+             'Here for you :)',
+             style: TextStyle(
+               fontFamily: 'Karla',
+               fontSize: 40,
+               color: Colors.black,
+             ),
+           ),
+           const SizedBox(height: 35),
+           GestureDetector(
+             onTap: () {Navigator.push(
+               context,
+               MaterialPageRoute(builder: (context) => BookRideScreen()),
+             );
+             },
+             child:Container(
+               width: 269,
+               height: 48,
+               decoration: BoxDecoration(
+                 color: Colors.black,
+                 borderRadius: BorderRadius.circular(7),
+               ),
+               child: const Center(
+                 child: Text(
+                   'Book a ride with us',
+                   style: TextStyle(
+                     fontFamily: 'Roboto',
+                     fontSize: 25,
+                     fontWeight: FontWeight.w500,
+                     color: Colors.white,
+                   ),
+                 ),
+               ),
+             ),
+           ),
 
-                const Spacer(),
-                const Divider(color: Colors.black, thickness: 1),
-                const SizedBox(height: 2),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.home, size: 31, color: Colors.black),
-                          onPressed: () {
-                            // Implement home functionality here
-                          },
-                        ),
-                        const SizedBox(height: 5),
-                        const Text(
-                          'Home',
-                          style: TextStyle(
-                            fontFamily: 'Roboto',
-                            fontSize: 14,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.history, size: 31, color: Colors.black),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => const RideHistoryScreen()),
-                            );
-                            // Implement history functionality here
-                          },
-                        ),
-                        const SizedBox(height: 5),
-                        const Text(
-                          'History',
-                          style: TextStyle(
-                            fontFamily: 'Roboto',
-                            fontSize: 14,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.person, size: 31, color: Colors.black),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) =>  AccountScreen()),
-                            );
-                          },
-                        ),
-                        const SizedBox(height: 5),
-                        const Text(
-                          'Account',
-                          style: TextStyle(
-                            fontFamily: 'Roboto',
-                            fontSize: 14,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 3),
-              ],
-            ),
-          ),
-        ),
+           const SizedBox(height: 60),
+           const Divider(color: Colors.black, thickness: 1),
+           const SizedBox(height: 2),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             children: [
+               Column(
+                 children: [
+                   IconButton(
+                     icon: const Icon(Icons.home, size: 31, color: Colors.black),
+                     onPressed: () {
+                       // Implement home functionality here
+                     },
+                   ),
+                   const SizedBox(height: 5),
+                   const Text(
+                     'Home',
+                     style: TextStyle(
+                       fontFamily: 'Roboto',
+                       fontSize: 14,
+                       color: Colors.black,
+                     ),
+                   ),
+                 ],
+               ),
+               Column(
+                 children: [
+                   IconButton(
+                     icon: const Icon(Icons.history, size: 31, color: Colors.black),
+                     onPressed: () {
+                       Navigator.push(
+                         context,
+                         MaterialPageRoute(builder: (context) => const RideHistoryScreen()),
+                       );
+                       // Implement history functionality here
+                     },
+                   ),
+                   const SizedBox(height: 5),
+                   const Text(
+                     'History',
+                     style: TextStyle(
+                       fontFamily: 'Roboto',
+                       fontSize: 14,
+                       color: Colors.black,
+                     ),
+                   ),
+                 ],
+               ),
+               Column(
+                 children: [
+                   IconButton(
+                     icon: const Icon(Icons.person, size: 31, color: Colors.black),
+                     onPressed: () {
+                       Navigator.push(
+                         context,
+                         MaterialPageRoute(builder: (context) =>  AccountScreen()),
+                       );
+                     },
+                   ),
+                   const SizedBox(height: 5),
+                   const Text(
+                     'Account',
+                     style: TextStyle(
+                       fontFamily: 'Roboto',
+                       fontSize: 14,
+                       color: Colors.black,
+                     ),
+                   ),
+                 ],
+               ),
+             ],
+           ),
+           const SizedBox(height: 3),
+         ],
+       ),
+       )
+      ),
+
 
     );
   }
